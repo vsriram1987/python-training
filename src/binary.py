00f657fb929ac0db1981e0cfd89b84df9f526e8d
Sriram <v.sriram.1987@gmail.com>
2019-08-12 23:36:36 +0530

    New python training timeit

---
+++ src/binary.py
@@ -0,0 +1,7 @@
+import timeit
+def mult1(val):
+    return str(val << 2)
+def mult2(val):
+    return str(val*4)
+print(timeit.timeit("-".join(map(mult1,range(100))),number=1000000))
+print(timeit.timeit("-".join(map(mult2,range(100))),number=1000000))
