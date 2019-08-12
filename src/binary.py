e96ff1ff1c484bdb42b39e5220ea6f47d5b46d51
Sriram <v.sriram.1987@gmail.com>
2019-08-12 23:39:20 +0530

    Binary Test

--- src/binary.py
+++ src/binary.py
@@ -4,4 +4,5 @@ def mult1(val):
 def mult2(val):
     return str(val*4)
 print(timeit.timeit("-".join(map(mult1,range(100))),number=1000000))
+
 print(timeit.timeit("-".join(map(mult2,range(100))),number=1000000))
