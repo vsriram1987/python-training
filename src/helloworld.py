b52c96aa921a39a00406bf4e2831bbe9315fa737
Sriram Venkat <v.sriram.1987@gmail.com>
2020-09-12 14:26:28 +0530

    message

--- src/helloworld.py
+++ src/helloworld.py
@@ -2,4 +2,4 @@ a = "The {animal_name} makes a {sound} sound."
 print(a.format(animal_name='dog',sound='barking'))
 print('thong'[:4])
 print('Py{a}'.format(a='thong'[:4]))
-print("hello world again...!")
+print("hello world again()...!")
